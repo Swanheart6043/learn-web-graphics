06173f7aaab8fc0dd85bd847086f455f0e61d546
Raphael <604332093@qq.com>
2022-11-22 11:28:42 +0800

    update

---
+++ src/LearnD3/first/hello.md
@@ -0,0 +1,14 @@
+几个常用api
+select 相当于document.querySelect()
+attr 设置属性
+
+x y cx cy
+width 宽
+height 高
+r 圆半径
+fill 内部颜色
+stroke 边框颜色
+transform 
+transform translate 平移
+transform rotate 旋转
+transform scale 缩放
---
+++ src/ThreeJS/index.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import THREE from "three";
+
+export function helloThree() {
+  useEffect(() => {
+    const Scene = new THREE.Scene()
+
+    const object = new THREE.Mesh()
+    
+    const d = new THREE.Camera()
+
+  }, [])
+}
