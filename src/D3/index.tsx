17c3521d95b0ff7a2fac3fd13fdb1325c33eb1ec
Raphael <huwh@yunji2025.com>
2022-11-23 16:24:49 +0800

    update

--- src/D3/index.tsx
+++ src/D3/index.tsx
@@ -1,9 +1,11 @@
 import ExploreD31 from "./explore-d3-1"
+import PanningAndZooming from "./interact/panning-and-zooming"
 
 export default function HelloD3() {
   return (
     <>
-      <ExploreD31 />
+      {/* <ExploreD31 /> */}
+      <PanningAndZooming />
     </>
   )
 }
---
+++ src/D3/interact/panning-and-zooming.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react"
+import * as d3 from "d3"
+
+export default function PanningAndZooming() {
+  const handleZoom = ({ transform }: any) => {
+    d3.selectAll("g").attr("transform", transform)
+  }
+
+  useEffect(() => {
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", 500)
+      .attr("height", 300)
+      .attr("transform", "translate(500, 200)")
+      .style("border", "1px solid #f0f0f0")
+
+    const g = svg.append("g")
+
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 5; j++) {
+        const res = g
+          .append("rect")
+          .attr("width", 20)
+          .attr("height", 20)
+          .attr("fill", "green")
+        const x = 20 * i + i * 10
+        const y = 20 * j + j * 10
+        res.attr("transform", `translate(${x}, ${y})`)
+      }
+    }
+
+    svg.call(
+      d3
+        .zoom()
+        .extent([
+          [0, 0],
+          [600, 600],
+        ])
+        .scaleExtent([1, 8])
+        .on("zoom", handleZoom) as any
+    )
+  }, [])
+
+  return null
+}
